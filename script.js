7486cabc1d7ba1a0843186133d5c08c24b3e41ad
Bob Scott <9009944+boscoru@users.noreply.github.com>
2025-11-16 19:14:20 -0500

    Completes project

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ const markerX = document.querySelector('#markerX');
 const markerO = document.querySelector('#markerO');
 const message = document.querySelector('#message');
 const restartButton = document.querySelector('#content > button');
+const dialogX = document.querySelector("#changeX");
+const showButtonX = document.querySelector("#nameX > button");
+const closeButtonX = document.querySelector("#changeX button");
+const dialogO = document.querySelector("#changeO");
+const showButtonO = document.querySelector("#nameO > button");
+const closeButtonO = document.querySelector("#changeO button");
+const submitX = document.getElementById('formX');
+const submitO = document.getElementById('formO');
+const gameBoard = document.getElementById('gameboard');
 
 let spots = [];
 for(i = 0; i<9; i++) spots[i] = document.getElementById(`${i}`);
@@ -41,10 +50,16 @@ let game = (function () {
             gameBoard[i] = [];
             for(let j = 0; j < 3; j++) gameBoard[i][j] = undefined;
         }
+        let line = document.querySelector('.line');
+        if (line) line.remove();
     };
 
     const checkWinner = () => {
-        winner = 1;
+        let stalemate = 1;
+        winner =1;
+        for (let i=0 ; i<3 ; i++) {
+            for (let j=0; j<3 ; j++) if(!gameBoard[i][j]) stalemate = 0;
+        }
         if (gameBoard[0][0] && gameBoard[0][0] === gameBoard[1][0] && gameBoard[1][0] === gameBoard[2][0]) return 1;
         else if(gameBoard[0][1] && gameBoard[0][1] === gameBoard[1][1] && gameBoard[1][1] === gameBoard[2][1]) return 2;
         else if(gameBoard[0][2] && gameBoard[0][2] === gameBoard[1][2] && gameBoard[1][2] === gameBoard[2][2]) return 3;
@@ -53,6 +68,10 @@ let game = (function () {
         else if(gameBoard[2][0] && gameBoard[2][0] === gameBoard[2][1] && gameBoard[2][1] === gameBoard[2][2]) return 6;
         else if(gameBoard[0][0] && gameBoard[0][0] === gameBoard[1][1] && gameBoard[1][1] === gameBoard[2][2]) return 7;
         else if(gameBoard[0][2] && gameBoard[0][2] === gameBoard[1][1] && gameBoard[1][1] === gameBoard[2][0]) return 8;
+        else if(stalemate === 1){
+            winner = 2;
+            return 0;
+        }
         else {
             winner = 0;
             return 0;
@@ -65,6 +84,62 @@ let game = (function () {
 
     return {xPlayer, oPlayer, getWinner, getTurn, takeTurn, getBoard, restart, checkWinner};
 })();
+
+function showWinner(position) {
+    let line = document.createElement('div')
+    line.classList.add('line');
+    switch (position) {
+        case 1:
+            line.style.top = '50%';
+            line.style.left = '16.667%';
+            line.style.transform = 'translate(-50%, -50%) rotate(90deg)';
+            line.style.width = '375px';
+            break;
+        case 2:
+            line.style.top = '50%';
+            line.style.left = '50%';
+            line.style.transform = 'translate(-50%, -50%) rotate(90deg)';
+            line.style.width = '375px';
+            break;
+        case 3:
+            line.style.top = '50%';
+            line.style.left = '83.333%';
+            line.style.transform = 'translate(-50%, -50%) rotate(90deg)';
+            line.style.width = '375px';
+            break;
+        case 4:
+            line.style.top = '16.667%';
+            line.style.left = '50%';
+            line.style.transform = 'translate(-50%, -50%)';
+            line.style.width = '375px';
+            break;
+        case 5:
+            line.style.top = '50%';
+            line.style.left = '50%';
+            line.style.transform = 'translate(-50%, -50%)';
+            line.style.width = '375px';
+            break;
+        case 6:
+            line.style.top = '83.333%';
+            line.style.left = '50%';
+            line.style.transform = 'translate(-50%, -50%)';
+            line.style.width = '375px';
+            break;
+        case 7:
+            line.style.top = '50%';
+            line.style.left = '50%';
+            line.style.transform = 'translate(-50%, -50%) rotate(45deg)';
+            line.style.width = '530px';
+            break;
+        case 8:
+            line.style.top = '50%';
+            line.style.left = '50%';
+            line.style.transform = 'translate(-50%, -50%) rotate(45deg)';
+            line.style.width = '530px';
+            break;
+    }
+    gameBoard.appendChild(line);
+}
 
 function playGame(e) {
     let choiceX, choiceY;
@@ -108,12 +183,18 @@ function playGame(e) {
                 break;
         }
         let result = game.takeTurn(choiceX, choiceY);  
-        game.checkWinner(); 
+        let position = game.checkWinner(); 
         if (result.success) {
             updateBoard(result.turn);
-            if (game.getWinner()) {
+            if (game.getWinner() === 1 ) {
                 if (result.turn === 'x') message.textContent = game.xPlayer + " wins!";
                 else message.textContent = game.oPlayer + " wins!";
+                markerX.style.backgroundColor = 'rgb(0, 0, 60)';
+                markerO.style.backgroundColor = 'rgb(0, 0, 60)';
+                showWinner(position);
+            }
+            else if (game.getWinner() ===2 ) {
+                message.textContent = "Stalemate!";
                 markerX.style.backgroundColor = 'rgb(0, 0, 60)';
                 markerO.style.backgroundColor = 'rgb(0, 0, 60)';
             }
@@ -143,8 +224,47 @@ function updateBoard(turn = 'o') {
     message.textContent = '';
 }
 
+function changeXName(event) {
+    event.preventDefault();
+    let form = document.getElementById(event.target.id);
+    let formData = new FormData(form);
+    showButtonX.textContent = formData.get('playerX');
+    game.xPlayer = formData.get('playerX');
+    form.reset();
+    dialogX.close();
+}
+
+function changeOName(event) {
+    event.preventDefault();
+    let form = document.getElementById(event.target.id);
+    let formData = new FormData(form);
+    showButtonO.textContent = formData.get('playerO');
+    game.oPlayer = formData.get('playerO');
+    form.reset();
+    dialogO.close();
+}
+
 board.addEventListener('click', playGame);
 restartButton.addEventListener('click', () => {
     game.restart();
     updateBoard();
 });
+
+showButtonX.addEventListener("click", () => {
+  dialogX.showModal();
+});
+
+closeButtonX.addEventListener("click", () => {
+  dialogX.close();
+});
+
+showButtonO.addEventListener("click", () => {
+  dialogO.showModal();
+});
+
+closeButtonO.addEventListener("click", () => {
+  dialogO.close();
+});
+
+submitX.addEventListener("submit", changeXName);
+submitO.addEventListener("submit", changeOName);
